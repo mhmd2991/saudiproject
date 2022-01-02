2dd84387be07df6aaf0191068473c5c42bd94195
Mohamad Salimi <mohamad.salimi@hotmail.com>
2022-01-02 14:42:05 +0200

    Add files via upload

--- js/radio.js
+++ js/radio.js
@@ -1,4 +1,4 @@
-let tab = document.querySelector(".nav li");
+/*let tab = document.querySelector(".nav li");
 let tabs = document.querySelectorAll(".nav li");
 let tabsArray = Array.from(tabs);
 let section = document.querySelectorAll(".section");
@@ -19,8 +19,50 @@ tabsArray.forEach((ele) => {
         });
         document.querySelector('#' + e.currentTarget.dataset.cont).classList.add("active");
     });
+});*/
+let currentSection = 0;
+let sections = document.querySelectorAll(".section");
+let sectionButtons = document.querySelectorAll(".nav > li");
+let nextButton = document.querySelector(".next");
+let previousButton = document.querySelector(".previous");
+for (let i = 0; i < sectionButtons.length; i++) {
+    sectionButtons[i].addEventListener("click", function() {
+        sections[currentSection].classList.remove("active");
+        sectionButtons[currentSection].classList.remove("active");
+        sections[currentSection = i].classList.add("active");
+        sectionButtons[currentSection].classList.add("active");
+        if (i === 0) {
+            if (previousButton.className.split(" ").indexOf("disable") < 0) {
+                previousButton.classList.add("disable");
+            }
+        } else {
+            if (previousButton.className.split(" ").indexOf("disable") >= 0) {
+                previousButton.classList.remove("disable");
+            }
+        }
+        if (i === sectionButtons.length - 1) {
+            if (nextButton.className.split(" ").indexOf("disable") < 0) {
+                nextButton.classList.add("disable");
+            }
+        } else {
+            if (nextButton.className.split(" ").indexOf("disable") >= 0) {
+                nextButton.classList.remove("disable");
+            }
+        }
+    });
+}
+
+nextButton.addEventListener("click", function() {
+    if (currentSection < sectionButtons.length - 1) {
+        sectionButtons[currentSection + 1].click();
+    }
 });
 
+previousButton.addEventListener("click", function() {
+    if (currentSection > 0) {
+        sectionButtons[currentSection - 1].click();
+    }
+});
 
 
 let drops = document.querySelectorAll('.drop');
